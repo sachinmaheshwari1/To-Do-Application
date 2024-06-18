e0a6fc735c1359568ca0a620f38009020bc09302
sachinmaheshwari1 <168063041+sachinmaheshwari1@users.noreply.github.com>
2024-06-18 09:47:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -52,15 +52,7 @@ function generateTimestamp() {
     return timestamp;
 }
 
-// Function to update the clock
-function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const formattedTime = `${hours}:${minutes}:${seconds}`;
-    document.querySelector('.clock').textContent = formattedTime;
-}
+ 
 
 // Update the clock every second
 setInterval(updateTime, 1000);
